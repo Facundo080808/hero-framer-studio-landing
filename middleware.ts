e652729e5ad92173ca7d48d507e81eae4bbdd228
ruanomiguel <162193452+ruanomiguel@users.noreply.github.com>
2025-05-10 14:36:36 -0500

    cambio de idioma inicial

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 const supportedLocales = ["es", "pt"]
-const defaultLocale = "es"
+const defaultLocale = "pt"
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
